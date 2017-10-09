267879999a5a1721b1a5162c88d1700dfca503bd
Oscar Casagrande <oscar.casagrande@hotmail.com.br>
2017-10-09 13:23:39 -0300

    adição de framework utils e database

---
+++ CasaFonteDeLuz/CasaFonteDeLuzAdocaoDB/Adocao.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Adocao]
+(
+	[IdCrianca] INT NOT NULL PRIMARY KEY, 
+    [emailPadrinho] VARCHAR(150) NULL, 
+    [nomePadrinho] VARCHAR(150) NULL, 
+    [comentarioPadrinho] VARCHAR(150) NULL, 
+    [telefonePadrinho] VARCHAR(20) NULL, 
+    [dataCriacao] DATETIME NOT NULL DEFAULT getdate()
+)
---
+++ CasaFonteDeLuz/CasaFonteDeLuzAdocaoDB/procAdocao_Ler.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[procAdocao_Ler]
+
+AS
+	SELECT
+		IdCrianca
+	,	emailPadrinho
+	,	nomePadrinho
+	,	comentarioPadrinho
+	,	telefonePadrinho
+	,	dataCriacao
+
+	FROM
+		dbo.Adocao
