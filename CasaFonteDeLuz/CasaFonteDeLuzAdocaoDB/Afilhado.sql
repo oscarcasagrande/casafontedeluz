f43c7ae765a5fb1d98382b7b175462e33afd4e0e
Oscar Casagrande <oscar.casagrande@hotmail.com.br>
2017-10-10 14:20:10 -0300

    script de pós deploy para inclusão de afilhados e proc para validar se afilhado está apadrinhado

---
+++ CasaFonteDeLuz/CasaFonteDeLuzAdocaoDB/Afilhado.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Afilhado]
+(
+	[IdAfilhado] CHAR(10) NOT NULL PRIMARY KEY, 
+    [dataCriacao] DATETIME NOT NULL DEFAULT getdate()
+)
---
+++ CasaFonteDeLuz/CasaFonteDeLuzAdocaoDB/procAfilhadoAdocao_Ler.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[procAfilhadoAdocao_Ler]
+AS
+	SELECT
+		Afilhado.IdAfilhado
+	,	case
+			when Adocao.IdAfilhado	is null then 0
+			else 1
+		end as Apdrinhado
+	FROM
+		dbo.Afilhado with(nolock)
+	left join
+		dbo.Adocao with(nolock)
+			on	Afilhado.IdAfilhado	=	Adocao.IdAfilhado
